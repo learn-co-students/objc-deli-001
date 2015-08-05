052f15b77a6bc23ee85d7612f5bd4967425bc831
vicfriedman <victoria.m.friedman@gmail.com>
2015-08-05 12:08:48 -0400

    tests passing

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -13,6 +13,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+//    NSString *result = [self stringWithDeliLine:@[@"Victoria", @"Mark"]];
+//    NSLog(@"%@", result);
+    
     return YES;
 }
 
@@ -21,5 +24,44 @@
  *  Define your methods here!
  
  */
+
+//stringWithDeliLine: should accept an NSMutableArray argument deliLine and return an NSString object.
+
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine{
+    if ([deliLine count] == 0){
+        return @"The line is currently empty.";
+    }
+    NSString *start = @"The line is:";
+    NSMutableString *final =[[NSMutableString alloc] init];
+    [final appendString:start];
+    NSLog(@"!!!!!!!!!!!!!!!!!!!! %@", final);
+    
+    
+    for (NSUInteger i = 0, j = 1; i < [deliLine count]; i++, j++){
+        NSString *sentence = [NSString stringWithFormat:@"\n%lu. %@", j, deliLine[i]];
+        [final appendString:sentence];
+    }
+         return final;
+}
+
+
+//addName:toDeliLine: should accept an NSString called name and an NSMutableArray called deliLine as arguments, and return an NSMutableArray.
+
+- (NSMutableArray *) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+
+//serveNextCustomerInDeliLine: should accept an NSMutableArray called deliLine as an argument and return an NSString.
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+    NSString *firstCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstCustomer;
+    
+}
+
+
 
 @end
