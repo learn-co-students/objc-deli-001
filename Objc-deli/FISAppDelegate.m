796ad272c5c1accc118d9c8ce658ce0546f062c7
Buntry <parkergriep@yahoo.com>
2015-08-05 18:51:14 -0400

    lab completed

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,10 +16,27 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine {
+    if ([deliLine count] != 0) {
+        NSString *line = @"The line is:";
+        for (int i=0; i < [deliLine count]; i++){
+            line = [line stringByAppendingString: [NSString stringWithFormat:@"\n%d. %@", i+1, deliLine[i]]];
+        }
+        return line;
+    }
+    
+    return @"The line is currently empty.";
+}
+
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *served = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return served;
+}
 
 @end
