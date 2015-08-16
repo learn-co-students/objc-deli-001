f7336b5bb48d9c4ca561705159a341fe0137600f
Joshua Motley <joshmotley12@172-16-36-225.DYNAPOOL.NYU.EDU>
2015-08-15 23:27:25 -0400

    possible

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -22,4 +22,32 @@
  
  */
 
+-(NSString *)stringWithDeliLine:(NSMutableArray *)deliLine{
+    
+    if(!deliLine){NSString *empty = @"The line is currently empty.";
+        return empty;}else{
+            NSMutableString *lineIs = [[NSMutableString alloc]initWithString:@"The line is: "];
+            for (int i =0; i<4; i++) {
+                NSString *number = [NSString stringWithFormat:@"%d ", i+1];
+                NSMutableString *mutableNumber = [number mutableCopy];
+                NSMutableString *itIs =[lineIs stringByAppendingString:mutableNumber];
+                NSMutableString *finished =[itIs stringByAppendingString:deliLine[i]];
+                return finished;
+                
+            }
+            
+        }
+    
+    return nil;};
+-(NSMutableArray *)addName:(NSString *)name: toDeliLine:(NSMutableArray *)deliLine{
+    [deliLine addObject:name];
+    return deliLine;
+};
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+    NSString *serving = [NSString stringWithFormat:@"%@", deliLine[0]];
+    [deliLine removeObjectAtIndex:0];
+    return serving;
+    
+};
+
 @end
