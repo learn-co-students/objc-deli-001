02725e976a09c8a06fb815c23de6f6b87d3504ec
linda9009 <linda.ng.9009@gmail.com>
2015-09-04 17:15:33 -0400

    Tests passed, probably inefficient code

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -17,5 +17,10 @@
  *  Declare your methods here!
  
  */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine;
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
 
 @end
