ca3bde209cb557a428d4402b5a509c2bd8122a27
joelconnects <joelconnects@gmail.com>
2015-09-07 14:15:12 -0400

    deli counter tests passing

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -22,4 +22,45 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSString *deliLineStatus;
+    
+    if ([deliLine count] == 0) {
+        deliLineStatus = @"The line is currently empty.";
+    } else {
+        
+        deliLineStatus = @"The line is:";
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            
+            NSUInteger deliNumber = i + 1;
+            NSString *deliNumberString = [@(deliNumber) stringValue];
+            
+            deliLineStatus = [deliLineStatus stringByAppendingString:@"\n"];
+            deliLineStatus = [deliLineStatus stringByAppendingString:deliNumberString];
+            deliLineStatus = [deliLineStatus stringByAppendingString:@". "];
+            deliLineStatus = [deliLineStatus stringByAppendingString:deliLine[i]];
+            
+        }
+    }
+    
+    return deliLineStatus;
+    
+}
+
+- (NSString *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [deliLine addObject:name];
+    
+    return name;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *nextCustomer = [deliLine objectAtIndex:0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return nextCustomer;
+}
+
 @end
