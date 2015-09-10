597d284dd2c63ce90f772bcaa69edcee08ee8082
newyork-anthonyng <baruchvita.anthonyng@gmail.com>
2015-09-10 09:44:44 -0400

    Deli Counter Updated 9/10/2015 Anthony Ng

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -22,4 +22,35 @@
  
  */
 
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSMutableString *peopleOnLine = [[NSMutableString alloc]init];
+    
+    if ([deliLine count] == 0) {
+        peopleOnLine = [NSMutableString stringWithString: @"The line is currently empty."];
+    } else {
+        peopleOnLine = [NSMutableString stringWithString:@"The line is:"];
+
+        // For every person on line, add their name to string
+        for(NSInteger i = 0; i < [deliLine count]; i++) {
+            NSString *currentCustomer = [NSMutableString stringWithFormat: @"\n%ld. %@", (i + 1), deliLine[i]];
+            peopleOnLine = (NSMutableString *)[peopleOnLine stringByAppendingString:currentCustomer];
+        }
+        
+    }
+
+    return peopleOnLine;
+}
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *currentCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return currentCustomer;
+}
+
+
 @end
