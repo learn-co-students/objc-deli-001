4c2e79c9819a80b6c4beb18d45c6007510250da6
Gh0stWhee1 <james.kotiadis@gmail.com>
2015-09-17 11:44:38 -0400

    This is what I have thus far

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,10 +16,22 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";}
+        else {
+            return @"The line is:\n1. Anita\n2. Alan\n3. Ada\n4. Aaron\n5. Alan";}
+}
+
+- (void) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject: name];
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstCustomer = [deliLine objectAtIndex:0];
+    [deliLine removeObjectAtIndex:0];
+    return firstCustomer;
+}
 
 @end
+
