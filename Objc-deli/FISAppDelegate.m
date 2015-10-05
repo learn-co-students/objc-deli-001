2bbb4b26025818e0360ed8d663bb5defb9ebd192
johnss518 <stacy.m.johnson@gmail.com>
2015-10-05 09:33:36 -0400

    add and remove from deli line

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,35 @@
     return YES;
 }
 
-/*
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
  
- *  Define your methods here!
- 
- */
+    if (!deliLine || [deliLine count] == 0) {
+        return @"The line is currently empty.";
+    } else {
+        NSMutableString *line = [@"The line is:" mutableCopy];
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            [line appendFormat:@"\n%lu. %@", i+1, deliLine[i]];
+        }
+        
+        return line;
+    }
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *nextCustomer = nil;
+    
+    if (deliLine && [deliLine count] != 0) {
+        nextCustomer = deliLine[0];
+        [deliLine removeObjectAtIndex:0];
+    }
+    
+    return nextCustomer;
+}
+
 
 @end
